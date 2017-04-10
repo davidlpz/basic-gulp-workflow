b6d7b1b31fa716fe3181355de76d08b75d60a01b
David <david@iMac-de-David.local>
2017-04-10 15:16:17 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# basic-gulp-workflow
-A basic gulp workflow to compile sass, add vendor prefixes and minify css and javascript
+Gulp Workflow
+======================
+A basic gulp workflow to compile sass, add vendor prefixes, minify css & javascript and live reload with browser sync.
+
+## Usage
+
+1. Clone this repository
+
+        $ git clone https://github.com/davidlpz/gulp-workflow.git
+
+2. Change the directory to the application folder
+
+        $ cd gulp-workflow
+
+3. Start a new project
+
+        $ npm init
+
+4. Install the dependencies
+
+        $ npm install gulp gulp-autoprefixer gulp-clean-css gulp-sass jshint gulp-jshint gulp-uglify browser-sync gulp-concat gulp-rename --save-dev
+
+5. Run the `default` task
+
+        $ gulp default
---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+
+// CSS related plugins
+var autoprefixer = require('gulp-autoprefixer');
+var cleancss = require('gulp-clean-css');
+var sass = require('gulp-sass');
+// JS related plugins
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+// Utility related plugins
+var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+var opt = {
+	src: 'src/',
+	dest: 'dist/',
+	width: 100
+}
+
+// Start browserSync server
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {
+			baseDir: 'dist'
+		},
+	});
+});
+
+// Copy HTML files
+gulp.task('html', function() {
+	return gulp.src(opt.src + '**/*.html')
+		.pipe(gulp.dest(opt.dest));
+});
+
+// Converts SASS to CSS with gulp-sass
+gulp.task('styles', function (){
+	return gulp.src(opt.src + 'sass/**/*.scss')
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		.pipe(autoprefixer({ cascade: false })) // Autoprefixer
+		.pipe(gulp.dest(opt.dest + 'css/'))
+		.pipe(rename({suffix: '.min'})) // Copy file
+		.pipe(cleancss()) // Minify CSS
+		.pipe(gulp.dest(opt.dest + 'css/'))
+		.pipe(browserSync.stream());
+});
+
+// Concatenate scripts and minify them into one output file
+gulp.task('scripts', function(){
+	return gulp.src(['src/js/plugins/*.js', 'src/js/main.js'])
+		.pipe(concat('main.js'))
+		.pipe(jshint())
+		.pipe(jshint.reporter('fail'))
+		.pipe(gulp.dest(opt.dest + 'js/'))
+		.pipe(rename({suffix: '.min'})) // Copy file
+		.pipe(uglify()) // Minify JS
+		.pipe(gulp.dest(opt.dest + 'js/'));
+});
+
+gulp.task('default', ['html', 'styles', 'scripts', 'browserSync'], function(){
+	gulp.watch('src/sass/*.scss', ['styles']);
+	gulp.watch('src/*.html', ['html', browserSync.reload]);
+	gulp.watch('src/js/*.js', ['scripts', browserSync.reload]);
+});
